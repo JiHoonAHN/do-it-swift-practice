a419f1d432a07c629ea728259100aa9dc79f38b0
JiHoonAHN <ahnjh1028@naver.com>
2021-02-22 01:34:41 +0900

    Initial Commit

---
+++ do it swift practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  do it swift practice
+//
+//  Created by Ji-hoon Ahn on 2021/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
